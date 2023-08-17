78ab1b21d3bd9792c0428f77016c9fdda7d05c05
Julius Matheus <juliusmatheusdesign@gmail.com>
2023-08-17 08:25:20 -0300

    Fechar menu ao clicar em link
    Funcionalidade de fechar o menu quando o usuário clica em um link foi adicionado

--- assets/scripts/menuOculto.js
+++ assets/scripts/menuOculto.js
@@ -1,22 +1,29 @@
 function menuMobile() {
-  var x = document.getElementById("nav-oculto");
-  var iconeMenu = document.getElementById("icone-menu");
+  var x = document.getElementById('nav-oculto')
+  var iconeMenu = document.getElementById('icone-menu')
 
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    iconeMenu.src = "./assets/images/icons/menu-open.svg";
+  if (x.style.display === 'block') {
+    x.style.display = 'none'
+    iconeMenu.src = './assets/images/icons/menu-open.svg'
   } else {
-    x.style.display = "block";
-    iconeMenu.src = "./assets/images/icons/menu-close.svg";
+    x.style.display = 'block'
+    iconeMenu.src = './assets/images/icons/menu-close.svg'
   }
 }
 
-document.addEventListener("click", function(event) {
-  var navOculto = document.getElementById("nav-oculto");
-  var iconeMenu = document.getElementById("icone-menu");
+document.addEventListener('click', function (event) {
+  var navOculto = document.getElementById('nav-oculto')
+  var iconeMenu = document.getElementById('icone-menu')
 
   if (!navOculto.contains(event.target) && event.target !== iconeMenu) {
-    navOculto.style.display = "none";
-    iconeMenu.src = "./assets/images/icons/menu-open.svg";
+    navOculto.style.display = 'none'
+    iconeMenu.src = './assets/images/icons/menu-open.svg'
   }
-});
+})
+
+var linksMenu = document.querySelectorAll('#nav-oculto a')
+for (var i = 0; i < linksMenu.length; i++) {
+  linksMenu[i].addEventListener('click', function () {
+    fecharMenu()
+  })
+}
